ffa64c2193fcf63621d97df7f60c620ab042f2cf
Steph W <stephanie.worladge@rmit.edu.au>
2025-02-08 00:10:56 +1100

    Initial Commit

---
+++ 2025YouAreAwesome/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  2025YouAreAwesome
+//
+//  Created by Steph on 8/2/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 2025YouAreAwesome/_025YouAreAwesomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  _025YouAreAwesomeApp.swift
+//  2025YouAreAwesome
+//
+//  Created by Steph on 8/2/2025.
+//
+
+import SwiftUI
+
+@main
+struct _025YouAreAwesomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
