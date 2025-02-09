583f74faafab25c1dc1b9301dcae63ff66ab5019
Steph W <stephanie.worladge@rmit.edu.au>
2025-02-09 22:25:51 +1100

    random lement variation of use with ranges instead of array.

--- 2025YouAreAwesome/ContentView.swift
+++ 2025YouAreAwesome/ContentView.swift
@@ -10,7 +10,9 @@ import SwiftUI
 struct ContentView: View {
     @State private var messageArray = ["You are fabulous", "Look at you go", "You are Awesome"]
     
-    @State private var imageName = ["image0","image1","image2","image3","image4","image5","image6","image7","image8","image9","image10"]
+//    @State private var imageName = ["image0","image1","image2","image3","image4","image5","image6","image7","image8","image9","image10"]
+    
+    @State private var imageName = ""
     
     @State private var imageNumber = 0
     @State private var message = ""
@@ -29,11 +31,14 @@ struct ContentView: View {
                 .foregroundStyle(.indigo)
             Spacer()
             Button("Show message ") {
-                imageNumber+=1
-                message = messageArray.randomElement() ?? ("No message in array")
-                if imageNumber == imageName.count {
-                    imageNumber = 0
-                }
+//                imageNumber+=1
+//                message = messageArray.randomElement() ?? ("No message in array")
+//                if imageNumber == imageName.count {
+//                    imageNumber = 0
+//                }
+                imageName = "image\(Int.random(in: 0...9))"
+                message = messageArray[Int.random(in: 0..<messageArray.count-1)]
+                imageNumber = (Int.random(in: 0...9))
             }
             .buttonStyle(.borderedProminent)
             .tint(.indigo)
